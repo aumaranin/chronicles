ac2f5abc8801ea351144ca425346647a5fa4f2c6
Маранин А.Ю <aumaranin@gmail.com>
2022-06-07 07:44:15 +0300

    new construtro Person

--- src/main/java/bmstu/chronicles/models/Person.java
+++ src/main/java/bmstu/chronicles/models/Person.java
@@ -22,6 +22,14 @@ public class Person
 
     private Boolean enabled;
 
+    private String gender;
+
+    private String phone;
+
+    private String relative_phone;
+
+    private String rank;
+
     public Person()
     {
 
@@ -38,6 +46,23 @@ public class Person
         this.role = role;
         this.login = login;
         this.enabled = enabled;
+    }
+
+    public Person(int person_id, String first_name, String second_name, String last_name, String date_of_birth, String password, String role, String login, Boolean enabled, String gender, String phone, String relative_phone, String rank)
+    {
+        this.person_id = person_id;
+        this.first_name = first_name;
+        this.second_name = second_name;
+        this.last_name = last_name;
+        this.date_of_birth = date_of_birth;
+        this.password = password;
+        this.role = role;
+        this.login = login;
+        this.enabled = enabled;
+        this.gender = gender;
+        this.phone = phone;
+        this.relative_phone = relative_phone;
+        this.rank = rank;
     }
 
     public int getPerson_id()
@@ -129,6 +154,22 @@ public class Person
     {
         this.enabled = enabled;
     }
+
+    public String getGender() { return gender; }
+
+    public void setGender(String gender) { this.gender = gender; }
+
+    public String getPhone() { return phone; }
+
+    public void setPhone(String phone) { this.phone = phone; }
+
+    public String getRelative_phone() { return relative_phone; }
+
+    public void setRelative_phone(String relative_phone) { this.relative_phone = relative_phone; }
+
+    public String getRank() { return rank; }
+
+    public void setRank(String rank) { this.rank = rank; }
 
     public String getRoleRus()
     {
